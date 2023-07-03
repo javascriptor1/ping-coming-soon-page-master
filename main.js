c16effd46ccf242f174100a6168fb829f509cc29
Mohammed Fakih <mhd.fakih@gmail.com>
2023-07-03 18:06:31 +0300

    finished javascript part

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+"use strict";
+
+const formControl = document.querySelector("#email-form");
+
+formControl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputEmail = document.querySelector('input[type="email"]').value;
+  validateEmail(inputEmail);
+});
+
+function validateEmail(email_id) {
+  const regex_pattern =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regex_pattern.test(email_id)) {
+    alert("Thank you. You have subscribed successfully. ☑");
+  } else {
+    alert("Please enter a valid email! 🚫");
+  }
+}
